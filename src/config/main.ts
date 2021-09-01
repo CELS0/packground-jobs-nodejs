50c5bd75ccd1d1fdb9e0876da0c7e5bc9de947a3
CELS0 <celsojr.300@gmail.com>
2021-09-01 16:55:36 -0300

    credentials

--- src/config/main.ts
+++ src/config/main.ts
@@ -4,8 +4,8 @@ const config = ({
     host: "smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "343e97f265cdbd",
-      pass: "ada09b45da2560"
+      user: "",
+      pass: ""
     }
   });
 
